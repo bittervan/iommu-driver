53c0595b10f9ef551310d29b8cff6f53360f273c
Xingkai Wang <xingkai.wang@outlook.com>
2024-03-06 19:12:50 +0800

    the pfn hash works!

--- drivers/iommu/riscv/mapping.c
+++ drivers/iommu/riscv/mapping.c
@@ -11,85 +11,71 @@
 uint64_t early_pgtbl[512] __attribute__((__aligned__(0x1000))) = {0};
 
 uint64_t swapper_pgtbl[512] __attribute__((__aligned__(0x1000))) = {0};
+#define IOVA_HASH_TABLE_SIZE (1024 * 256)
 
-// avl tree is too slow! try hash table
-// struct iova_node* iova_tree_root = NULL;
-// Define the hash node structure
-struct iova_hash_node {
-    uint64_t iova;
-    uint8_t cnt;
-    struct hlist_node hlist;
-};
+// It seems using the PFN is the best performance! There will be no collision at all!
+// Use hash table to maintain the reference count of each iova
+// uint64_t customHash(uint64_t input) {
+//     uint64_t hash = input;
+//     hash = (hash ^ (hash >> 27)) + (hash % 997);
+//     return hash;
+// }
 
-// Define the hash table
-DEFINE_HASHTABLE(iova_hash_table, 16);  // 10 buckets
+// first byte is reference count, the rest are iova;
+uint64_t hash_table[IOVA_HASH_TABLE_SIZE] = {0};
 
-// Initialize the hash table
-void init_iova_hash_table(void) {
-    // No additional initialization needed; the DEFINE_HASHTABLE macro takes care of it
-}
+uint8_t insert_into_iova_hash_table(uint64_t iova) {
+    uint64_t hash = (iova >> 12) % IOVA_HASH_TABLE_SIZE;
+    uint64_t ref_cnt;
 
-// Insert a node into the hash table
-int insert_into_iova_hash_table(uint64_t iova) {
-    struct iova_hash_node *new_node;
-    struct iova_hash_node *existing_node;
-
-    // Allocate a new node
-    new_node = kmalloc(sizeof(struct iova_hash_node), GFP_KERNEL);
-    if (!new_node)
-        return -ENOMEM;
-
-    // Initialize the new node
-    new_node->iova = iova;
-    new_node->cnt = 1;  // Initial count is 1
-
-    // Check if a node with the same iova already exists
-    hash_for_each_possible(iova_hash_table, existing_node, hlist, iova) {
-        if (existing_node->iova == iova) {
-            // Iova already exists, increase the count and free the new node
-            existing_node->cnt++;
-            kfree(new_node);
-            return 0;
-        }
+    while ((hash_table[hash] != 0) && (hash_table[hash] & 0x00FFFFFFFFFFFFFF) != iova) {
+        hash = (hash + 1) % IOVA_HASH_TABLE_SIZE;
     }
 
-    // Insert the new node into the hash table
-    hash_add(iova_hash_table, &new_node->hlist, iova);
+    ref_cnt = hash_table[hash] >> 56;
+    if (ref_cnt == 0) {
+        hash_table[hash] = (1UL << 56) | iova;
+    } else {
+        hash_table[hash] = ((ref_cnt + 1) << 56) | iova;
+    }
 
-    return 0;
+    return ref_cnt + 1;
 }
 
-// Remove a node from the hash table
-void remove_from_iova_hash_table(uint64_t iova) {
-    struct iova_hash_node *node;
+uint8_t remove_from_iova_hash_table(uint64_t iova) {
+    uint64_t hash = (iova >> 12) % IOVA_HASH_TABLE_SIZE;
+    uint64_t ref_cnt;
 
-    // Find the node with the specified iova
-    hash_for_each_possible(iova_hash_table, node, hlist, iova) {
-        if (node->iova == iova) {
-            // Decrease the count
-            if (--node->cnt == 0) {
-                // Count becomes zero, remove the node from the hash table and free it
-                hash_del(&node->hlist);
-                kfree(node);
-            }
-            break;
-        }
+    while ((hash_table[hash] != 0) && (hash_table[hash] & 0x00FFFFFFFFFFFFFF) != iova) {
+        hash = (hash + 1) % IOVA_HASH_TABLE_SIZE;
+    }
+
+    ref_cnt = hash_table[hash] >> 56;
+
+    if (ref_cnt == 0) {
+        return 0;
+    } else
+
+    if (ref_cnt == 1) {
+        hash_table[hash] = 0;
+        return ref_cnt - 1;
+    } else {
+        hash_table[hash] = ((ref_cnt - 1) << 56) | iova;
+        return ref_cnt - 1;
     }
 }
 
-uint8_t get_cnt_for_iova(uint64_t iova) {
-    struct iova_hash_node *node;
+// uint8_t get_cnt_for_iova(uint64_t iova) {
+//     uint64_t hash = customHash(iova) % IOVA_HASH_TABLE_SIZE;
+//     uint64_t ref_cnt;
 
-    // Find the node with the specified iova
-    hash_for_each_possible(iova_hash_table, node, hlist, iova) {
-        if (node->iova == iova) {
-            return node->cnt;
-        }
-    }
+//     while ((hash_table[hash] != 0) && (hash_table[hash] & 0x00FFFFFFFFFFFFFF) != iova) {
+//         hash = (hash + 1) % IOVA_HASH_TABLE_SIZE;
+//     }
 
-    // Node not found, return 0 or an appropriate default value
-    return 0;
-}
+//     ref_cnt = hash_table[hash] >> 56;
+//     return ref_cnt;
+// }
 
 void riscv_iommu_create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t size, uint64_t flags) {
     uint64_t num_pages;
@@ -97,20 +83,7 @@ void riscv_iommu_create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint6
     pa = PGROUNDDOWN(pa);
     num_pages = (PGROUNDUP(va + size) - va) / PGSIZE;
 
-    // printk("riscv_iommu_create_mapping: va: %llx, pa: %llx, size: %llx, flags: %llx\n", va, pa, size, flags);
-
-
-    // iova_tree_root = iova_node_insert(iova_tree_root, va);
-    // if (iova_node_get_count(iova_tree_root, va) != 1) {
-        // return;
-    // }
-
     // insert the mapping into the iova hash table
-    insert_into_iova_hash_table(va);
-    if (get_cnt_for_iova(va) != 1) {
-        return;
-    }
-
     for (int i = 0; i < num_pages; i++) {
         uint64_t current_va = va + i * PGSIZE;
         uint64_t current_pa = pa + i * PGSIZE;
@@ -122,7 +95,9 @@ void riscv_iommu_create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint6
         uint64_t l2_entry;
         uint64_t *l3_pgtbl;
 
-
+        if (insert_into_iova_hash_table(current_va) != 1) {
+            return;
+        }
 
         // printk("before accessing pgtbl, pgtbl: %016llx, index1: %lld\n", (uint64_t)pgtbl, index1);
         if (pgtbl[index1] == 0) {
@@ -173,11 +148,6 @@ void riscv_iommu_remove_mapping(uint64_t *pgtbl, uint64_t va, uint64_t size) {
     // if (iova_node_get_count(iova_tree_root, va) != 0) {
     //     return;
     // }
-    remove_from_iova_hash_table(va);
-    if (get_cnt_for_iova(va) != 0) {
-        return;
-    }
-
     for (int i = 0; i < num_pages; i++) {
         uint64_t current_va = va + i * PGSIZE;
         uint64_t index1 = VA_VPN2(current_va);
@@ -187,6 +157,10 @@ void riscv_iommu_remove_mapping(uint64_t *pgtbl, uint64_t va, uint64_t size) {
         uint64_t *l2_pgtbl;
         uint64_t l2_entry;
         uint64_t *l3_pgtbl;
+
+        if (remove_from_iova_hash_table(current_va) != 0) {
+            return;
+        }
 
         if (pgtbl[index1] == 0) {
             continue;
@@ -245,205 +219,3 @@ uint64_t io_to_virt(uint64_t pa) {
 uint64_t virt_to_io(uint64_t va) {
     return virt_to_phys((void*)va);
 }
-
-// struct iova_node {
-//     uint8_t cnt;
-//     int height;
-//     uint64_t iova;
-//     struct iova_node *left;
-//     struct iova_node *right;
-// };
-
-// struct iova_node *iova_tree_root;
-
-// struct iova_node *rotate_node_to_top(struct iova_node* accessed_node) {
-//     struct iova_node *parent_node = NULL;
-//     struct iova_node *current_node = iova_tree_root;
-//     while (1) {
-//         if (current_node == accessed_node) {
-//             break;
-//         } else if (accessed_node->iova < current_node->iova) {
-//             struct iova_node *left_node = current_node->left;
-//             if (left_node == accessed_node) {
-//                 if (parent_node == NULL) {
-//                     iova_tree_root = accessed_node;
-//                 } else if (parent_node->left == current_node) {
-//                     parent_node->left = accessed_node;
-//                 } else {
-//                     parent_node->right = accessed_node;
-//                 }
-//                 current_node->left = accessed_node->right;
-//                 accessed_node->right = current_node;
-//                 break;
-//             } else {
-//                 parent_node = current_node;
-//                 current_node = left_node;
-//             }
-//         } else {
-//             struct iova_node *right_node = current_node->right;
-//             if (right_node == accessed_node) {
-//                 if (parent_node == NULL) {
-//                     iova_tree_root = accessed_node;
-//                 } else if (parent_node->left == current_node) {
-//                     parent_node->left = accessed_node;
-//                 } else {
-//                     parent_node->right = accessed_node;
-//                 }
-//                 current_node->right = accessed_node->left;
-//                 accessed_node->left = current_node;
-//                 break;
-//             } else {
-//                 parent_node = current_node;
-//                 current_node = right_node;
-//             }
-//         }
-//     }
-//     return accessed_node;
-// }
-
-// bool remove_iova(uint64_t current_va) {
-//     // remove the mapping if the reference count is zero
-//     struct iova_node *current_node = iova_tree_root;
-//     struct iova_node *parent_node = NULL;
-//     bool should_delete = 0;
-
-//     while (1) {
-//         if (current_va < current_node->iova) {
-//             parent_node = current_node;
-//             current_node = current_node->left;
-//         } else if (current_va > current_node->iova) {
-//             parent_node = current_node;
-//             current_node = current_node->right;
-//         } else {
-//             current_node->cnt--;
-//             if (current_node->cnt == 0) {
-//                 should_delete = 1;
-//                 if (current_node->left == NULL && current_node->right == NULL) {
-//                     if (parent_node == NULL) {
-//                         iova_tree_root = NULL;
-//                     } else if (parent_node->left == current_node) {
-//                         parent_node->left = NULL;
-//                     } else {
-//                         parent_node->right = NULL;
-//                     }
-//                     kfree(current_node);
-//                 } else if (current_node->left == NULL) {
-//                     if (parent_node == NULL) {
-//                         iova_tree_root = current_node->right;
-//                     } else if (parent_node->left == current_node) {
-//                         parent_node->left = current_node->right;
-//                     } else {
-//                         parent_node->right = current_node->right;
-//                     }
-//                     kfree(current_node);
-//                 } else if (current_node->right == NULL) {
-//                     if (parent_node == NULL) {
-//                         iova_tree_root = current_node->left;
-//                     } else if (parent_node->left == current_node) {
-//                         parent_node->left = current_node->left;
-//                     } else {
-//                         parent_node->right = current_node->left;
-//                     }
-//                     kfree(current_node);
-//                 } else {
-//                     struct iova_node *successor = current_node->right;
-//                     struct iova_node *successor_parent = current_node;
-//                     while (successor->left != NULL) {
-//                         successor_parent = successor;
-//                         successor = successor->left;
-//                     }
-//                     current_node->iova = successor->iova;
-//                     current_node->cnt = successor->cnt;
-//                     if (successor_parent == current_node) {
-//                         successor_parent->right = successor->right;
-//                     } else {
-//                         successor_parent->left = successor->right;
-//                     }
-//                     kfree(successor);
-//                 }
-//             }
-//             break;
-//         }
-//     }
-
-//     return should_delete;
-// }
-
-// struct iova_node *insert_iova(uint64_t current_va) {
-//     if (unlikely(iova_tree_root == NULL)) {
-//         iova_tree_root = (struct iova_node *)kmalloc(sizeof(struct iova_node), GFP_KERNEL);
-//         iova_tree_root->iova = current_va;
-//         iova_tree_root->cnt = 1;
-//         iova_tree_root->left = NULL;
-//         iova_tree_root->right = NULL;
-//         return iova_tree_root;
-//     } else {
-//         struct iova_node *current_node = iova_tree_root;
-//         while (1) {
-//             if (current_va < current_node->iova) {
-//                 if (current_node->left == NULL) {
-//                     current_node->left = (struct iova_node *)kmalloc(sizeof(struct iova_node), GFP_KERNEL);
-//                     current_node->left->iova = current_va;
-//                     current_node->left->cnt = 1;
-//                     current_node->left->left = NULL;
-//                     current_node->left->right = NULL;
-//                     return current_node->left;
-//                 } else {
-//                     current_node = current_node->left;
-//                 }
-//             } else if (current_va > current_node->iova) {
-//                 if (current_node->right == NULL) {
-//                     current_node->right = (struct iova_node *)kmalloc(sizeof(struct iova_node), GFP_KERNEL);
-//                     current_node->right->iova = current_va;
-//                     current_node->right->cnt = 1;
-//                     current_node->right->left = NULL;
-//                     current_node->right->right = NULL;
-//                     return current_node->right;
-//                 } else {
-//                     current_node = current_node->right;
-//                 }
-//             } else {
-//                 current_node->cnt++;
-//                 return current_node;
-//             }
-//         }
-//     }
-//     // if (unlikely(iova_tree_root == NULL)) {
-//     //     iova_tree_root = (struct iova_node *)kmalloc(sizeof(struct iova_node), GFP_KERNEL);
-//     //     iova_tree_root->iova = current_va;
-//     //     iova_tree_root->cnt = 1;
-//     //     iova_tree_root->left = NULL;
-//     //     iova_tree_root->right = NULL;
-//     // } else {
-//     //     struct iova_node *current_node = iova_tree_root;
-//     //     while (1) {
-//     //         if (current_va < current_node->iova) {
-//     //             if (current_node->left == NULL) {
-//     //                 current_node->left = (struct iova_node *)kmalloc(sizeof(struct iova_node), GFP_KERNEL);
-//     //                 current_node->left->iova = current_va;
-//     //                 current_node->left->cnt = 1;
-//     //                 current_node->left->left = NULL;
-//     //                 current_node->left->right = NULL;
-//     //                 break;
-//     //             } else {
-//     //                 current_node = current_node->left;
-//     //             }
-//     //         } else if (current_va > current_node->iova) {
-//     //             if (current_node->right == NULL) {
-//     //                 current_node->right = (struct iova_node *)kmalloc(sizeof(struct iova_node), GFP_KERNEL);
-//     //                 current_node->right->iova = current_va;
-//     //                 current_node->right->cnt = 1;
-//     //                 current_node->right->left = NULL;
-//     //                 current_node->right->right = NULL;
-//     //                 break;
-//     //             } else {
-//     //                 current_node = current_node->right;
-//     //             }
-//     //         } else {
-//     //             current_node->cnt++;
-//     //             break;
-//     //         }
-//     //     }
-//     // }
-// }
-
